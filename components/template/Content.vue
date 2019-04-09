2e454cbc16fd2823eaf54d8fd7f9c6f19a569869
Sidiney <sidineysilva1@gmail.com>
2019-04-09 11:35:56 -0300

    First commit

---
+++ components/template/Content.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="content">
+		<slot />		
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Content'
+	}
+</script>
+
+<style>
+	.content{
+		grid-area: content;
+		background-color: #efefef;
+		padding: 20px;
+	}
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <app-header />
+    <app-menu />
+    <app-content>
+        <nuxt />
+    </app-content>
+    <app-footer />
+  </div>
+</template>
+
+<script>
+    import Header from '@/components/template/Header'
+    import Menu from '@/components/template/Menu'
+    import Content from '@/components/template/Content'
+    import Footer from '@/components/template/Footer'
+
+    export default{
+        components:{
+          appHeader: Header,
+          appMenu: Menu,
+          appContent: Content,
+          appFooter: Footer
+        }
+    }
+</script>
+
+<style>
+
+    *{
+      font-family: "Lato", sans-serif;
+    }
+    body{
+      margin: 0;
+    }
+
+    #app{
+      -webkit-font-smoothig: antialiaed;
+      -moz-osx-font-smoothig: grayscale;
+
+      height: 100vh;
+      display: grid;
+      grid-template-rows: 60px 1fr 40px;
+      grid-template-columns: 300px 1fr;
+      grid-template-areas: 
+          "header header"
+          "menu content"
+          "menu footer";
+    }
+</style>
