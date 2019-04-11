101d5e638bfcd2cf97eaf61fe628d0cfed49d308
Sidiney <sidineysilva1@gmail.com>
2019-04-11 11:10:45 -0300

    Adicionei o component Stat e ja o implementei no Home

---
+++ components/general/Stat.vue
@@ -0,0 +1,78 @@
+<template>
+	<div class="stat">
+		<div class="stat-icon">
+			<i :class="icon" :style="color"></i>
+		</div>
+		<div class="stat-info">
+			<span class="stat-title">{{ title }}</span>
+			<span class="stat-value">{{ value }}</span>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		name: 'Stat',
+		props: {
+			title: {
+				required: true,
+				type: String
+			},
+			value: {
+				required: true,
+				type: Number,
+				default: 0
+			},
+			icon: String,
+			color: {
+				type: [String, Object],
+				default: () => ({color: '#000'})
+			}
+			
+
+		},
+		// computed: {
+		// 	style(){
+		// 		return `color: ${this.color || '#000'}`
+		// 	}
+		// }
+	}
+</script>
+
+<style>
+	.stat {
+		flex: 1;
+		display: flex;
+		border-radius: 8px;		
+		margin-right: 20px;
+		margin-bottom: 20px;
+		background-color: #FFF;
+		padding: 20px;
+		border: 1px solid rgba(0, 0, 0, 0.2);
+		box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
+	}
+
+	.stat-icon{
+		display: flex;
+		align-items: center;
+	}
+
+		.stat-icon i {
+			font-size: 3rem;
+		}
+
+	.stat-info{
+		flex: 1;
+		display: flex;
+		flex-flow: column;
+		align-items: flex-end;
+	}
+
+	.stat-title {
+		font-size: 1rem;
+	}
+
+	.stat-value {
+		font-size: 2rem;
+	}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,15 +5,63 @@
 			main="Dashboard"
 			sub="Base de conhecimento"
 		/>
+		<div class="stats">
+			<Stat 
+				title="Categorias"
+				:value="stat.categories"
+				icon="fa fa-folder"
+				color="color: #d54d50"
+			/>
+			<Stat 
+				title="Artigos"
+				:value="stat.articles"
+				icon="fa fa-file"
+				color="color: #3bc480"
+			/>
+			<Stat 
+				title="Usuários"
+				:value="stat.users"
+				icon="fa fa-user"
+				color="color: #3282cd"
+			/>
+		</div>
 	</div>
 </template>
 
 <script>
+	import Stat from '@/components/general/Stat'
 	export default{
-		name: 'Home'
+		name: 'Home',
+		components: {
+			Stat
+		},
+		data: () => ({
+			stat: {}
+		}),
+		methods: {
+			getStats(){
+				//Aqui estou mockando uma requisição ao servidor
+				return new Promise(resolve => {
+					const stat = {
+						categories: 3,
+						articles: 5,
+						users: 2
+					}
+
+					setTimeout(() => resolve(stat), 3000)
+				})
+			}
+		},
+		async created(){
+			this.stat = await this.getStats()
+		}
 	}
 </script>
 
 <style>
-
+	.stats{
+		display: flex;
+		justify-content: space-between;
+		flex-flow: wrap;
+	}
 </style>
