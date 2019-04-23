54f797b13ec9d2b37629232f8c88f5694ba91553
Sidiney <sidineysilva1@gmail.com>
2019-04-23 16:04:52 -0300

    Inicie o componente de exibição da categoria

---
+++ pages/categories/_categoryId/articles.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="articles-by-category">
+		<PageTitle 
+			icon="far fa-folder" 
+			:main="category.name"
+			sub="Categoria"
+			/>
+	</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		name: 'ArticleByCategory',
+		data: () => ({
+			category: {},
+			articles: [],
+			page: 1,
+			loadMore: true
+		}),
+		mounted(){
+			this.category.id = this.$route.params.categoryId 
+			this.getCategory()
+		},
+		methods: {
+			getCategory(){
+				console.log(this.category)
+			}
+		}	
+	}
+</script>
+
+<style>
+	.stats{
+		display: flex;
+		justify-content: space-between;
+		flex-flow: wrap;
+	}
+</style>
