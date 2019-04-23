cc17e9de8e1cca9d99913e9422a51a7aa1b9ed19
Sidiney <sidineysilva1@gmail.com>
2019-04-23 16:54:13 -0300

    Tanto a rota como o componente de article já estão funcionando

--- pages/categories/_categoryId/articles.vue
+++ pages/categories/_categoryId/articles.vue
@@ -1,10 +1,23 @@
 <template>
 	<div class="articles-by-category">
-		<PageTitle 
+		<PageTitle
 			icon="far fa-folder" 
 			:main="category.name"
 			sub="Categoria"
 			/>
+		<ul>
+			<li v-for="article in articles" :key="article.id">
+				{{ article.name }}
+			</li>
+		</ul>
+		<div class="load-more">
+			<button 
+				v-if="loadMore" 
+				@click="getArticles" 
+				class="btn btn-lg btn-outline-primary">
+					Carregar Mais Artigos
+			</button>
+		</div>
 	</div>
 	</div>
 </template>
@@ -18,22 +31,51 @@
 			page: 1,
 			loadMore: true
 		}),
-		mounted(){
+		created(){
 			this.category.id = this.$route.params.categoryId 
 			this.getCategory()
+			this.getArticles()
 		},
 		methods: {
 			getCategory(){
+				this.category.name = 'Web Moderno'
 				console.log(this.category)
+			},
+			getArticles(){
+				const articles = [
+					{
+						id: Math.floor(Math.random() * 10000),
+						name: 'ABC '
+					},
+					{
+						id: Math.floor(Math.random() * 10000),
+						name: 'DEF'
+					},
+					{
+						id: Math.floor(Math.random() * 10000),
+						name: 'HIJ'
+					},
+				]
+				this.articles = [...this.articles, ...articles]
+				this.page++
+
+				if(this.articles.length > 10)
+					this.loadMore = false
 			}
 		}	
 	}
 </script>
 
 <style>
-	.stats{
+	.articles-by-category ul{
+		list-style-type: none;
+		padding: 0px;
+	}
+
+	.articles-by-category .load-more{
 		display: flex;
-		justify-content: space-between;
-		flex-flow: wrap;
+		flex-flow: column;
+		align-items: center;
+		margin-top: 25px;
 	}
 </style>
